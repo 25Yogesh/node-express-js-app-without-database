d1f26ee0e32d4421b254c13d44b5ec607f8a676b
Yogesh Sahu <ysahu25071999@gmail.com>
2025-04-30 11:23:17 +0530

    FIRST COMMIT

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+//  This helps in parsing text kind of data in json format
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//  This helps to make public folder as accesable directory
+//  Now we can use /css/man.css directl as it will be serched
+//  in public folder only
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.rouutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
